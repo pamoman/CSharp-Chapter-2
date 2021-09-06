9c371af3536c06ffd9e00d98c00e2f4d417beff3
Paul Moreland <Paul.Moreland@edu.lidkoping.se>
2021-09-06 14:11:21 +0200

    Changed title

--- CSharp-Chapter-2/Program.cs
+++ CSharp-Chapter-2/Program.cs
@@ -6,7 +6,7 @@ namespace CSharp_Chapter_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Chapter 2");
         }
     }
 }
